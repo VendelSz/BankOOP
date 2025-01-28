b68d490f19a10fa9e26fc1b56a659e6dc7a2ae80
Diák <Dik@PETRIK>
2025-01-28 09:15:09 +0100

    Add project files.

---
+++ BankOOP/Bank.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOOP
+{
+	internal class Bank
+	{
+		//attribútumok
+		private List<BankSzamla> szamlak = new List<BankSzamla>();
+
+		//új számla létrehozása
+		public void SzamlaLetrehoz(string tulajdonosNev, int kezdoEgyenleg)
+		{
+			BankSzamla ujSzamla = new BankSzamla(tulajdonosNev, kezdoEgyenleg);
+			szamlak.Add(ujSzamla);
+            Console.WriteLine($"Új számla létrehozva! {ujSzamla.GetSzamlaAdatok()}");
+        }
+
+		//számla keresés
+		public BankSzamla SzamlaKeres(string szamlaszam)
+		{
+			foreach (var szamla in szamlak)
+			{
+				if (szamla.SzamlaSzam == szamlaszam)
+				{
+					return szamla;
+				}
+			}
+			return null;
+		}
+	}
+}
---
+++ BankOOP/BankSzamla.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOOP
+{
+	internal class BankSzamla
+	{
+		//Osztály attributumok
+		//statikus változó lényege, hogy nem változik
+		private static int kovetkezoSzamlaSzam = 1000;
+		public string SzamlaSzam { get; private set; }
+		public string TulajdonosNev { get; private set; }
+		public int Egyenleg { get; private set; }
+
+		//konstruktor
+		//init -> kezdeti értéket adunk a változóknak
+		public BankSzamla(string tulajdonosNev, int kezdoEgyenleg)
+		{
+			SzamlaSzam = $"ACC{kovetkezoSzamlaSzam++}";
+			TulajdonosNev = tulajdonosNev;
+			Egyenleg = kezdoEgyenleg;
+		}
+
+		//Befizet metódus
+		public void Befizet(int osszeg)
+		{
+			if (osszeg > 0)
+			{
+				Egyenleg += osszeg;
+				Console.WriteLine($"{osszeg} HUF befizetve a(z) {SzamlaSzam} számlára");
+			} else
+			{
+                Console.WriteLine("Hibás összeg, nem lehet negatív vagy nulla.");
+            }
+		}
+
+		//Kifizet metódus
+		public void Kifizet(int osszeg)
+		{
+			if (osszeg > 0 && osszeg <= Egyenleg)
+			{
+				Egyenleg -= osszeg;
+                Console.WriteLine($"{osszeg} HUF kifizetve a(z) {SzamlaSzam} számláról");
+			} else
+			{
+				Console.WriteLine("Hibás összeg, vagy nincs elég pénz a számlán.");
+			}
+		}
+
+		//számla adatok lekérdezése
+		public string GetSzamlaAdatok()
+		{
+			return $"Számlaszám: {SzamlaSzam}, Tulajdonos: {TulajdonosNev}, Egyenleg: {Egyenleg}HUF.";
+		}
+	}
+}
